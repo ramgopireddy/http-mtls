863ada978fb1b295bf1a92b3d8a9eaccd5fef9f1
Ramakanth Gopireddy <44591730+ramgopireddy@users.noreply.github.com>
2021-05-18 13:19:24 -0400

    Update client.js

--- client.js
+++ client.js
@@ -3,11 +3,11 @@ const https = require('https');
 let str = "";
 const req = https.request(
   {
-    hostname: 'http-mtls.apps.cluster-6c24.6c24.example.opentlc.com',
+    hostname: 'http-mtls.apps.cluster-4314.4314.sandbox1764.opentlc.com',
     port: 443,
     path: '/',
     method: 'GET',
-    servername: 'http-mtls.apps.cluster-6c24.6c24.example.opentlc.com',
+    servername: 'http-mtls.apps.cluster-4314.4314.sandbox1764.opentlc.com',
     cert: fs.readFileSync('./ingress/certs/client/client.crt'),
     key: fs.readFileSync('./ingress/certs/client/client.key'),
     ca: fs.readFileSync('./ingress/certs/ca/ca.crt'),
